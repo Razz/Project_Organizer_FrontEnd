ca439ab3a745706bd17d738e2d008152044af0c3
Carlos Nunez <carlosonunez@noreply.github.com>
2020-04-14 20:33:13 -0500

    Add instrumentation
    Hook our React app up to Prometheus.
    
    Rant time.
    
    It is MIND-BOGGLING that there are no tutorials on how to do this. I
    spent two days hacking the entire Earth to find a way of adding the
    `/metrics` route to this app, including:
    
    - Adding it as a component (HA HA; React is for SPAs, so that's a no-dice),
    - Manually crafting a `<Route>` object (See the first bullet),
    - Ejecting the application's configuration using `npm run eject` and
      adding it to the webpack config directly (to only learn that webpack
      just compiles everything into static assets and bundles them, even
      though it is _also_ somehow responsible for starting a web
      server...somewhere), and
    - Unejecting (it's irreversible...if you don't know Git) the app,
      adding a `serverProxy.js` and having the proxy create the metrics via
      interception (which doesn't work because the proxy keeps routing to
      itself, as you would expect).
    
    It turns out that all you need to do is compile the app with `npm
    build`, then create an Express config at the root of the project, point
    it at `index.html` and run `node server.js` (assuming the file is called
    `server.js`).
    
    It was only after adding the `/metrics` route there did I learn that
    `express-prom-bundle` exists which creates the exporter for you
    extremely easily.
    
    You know a blog post is coming to address this.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ MAINTAINER Carlos Nunez <dev@carlosnunez.me>
 
 COPY . /app
 WORKDIR /app
-RUN npm install && npm install -g react-scripts
+RUN npm set progress=false && \
+  npm install --prefer-offline && \
+  npm install -g react-scripts
 
 USER nobody
 ENTRYPOINT npm start
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const prometheus = require('express-prom-bundle')
+const metricsMiddleware = prometheus({
+  includeMethod: true,
+  includePath: true,
+  promClient: { collectDefaultMetrics: {} }
+})
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(metricsMiddleware);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
