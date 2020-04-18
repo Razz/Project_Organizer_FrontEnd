5a075ea53365a4fba294ca42138cc04cc54a1e81
Carlos Nunez <carlosonunez@noreply.github.com>
2020-04-18 15:09:41 -0500

    Duh.
    Because we are manually taking care of standing up the Express web
    server, we can add our proxy configuration directly into `server.js`
    instead of relying on CRA  black magic to stand up the proxy for us.
    
    The things you miss when you're tired af.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const prometheus = require('express-prom-bundle')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const metricsMiddleware = prometheus({
   includeMethod: true,
   includePath: true,
@@ -17,6 +18,18 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(metricsMiddleware);
+app.use('/tracing', createProxyMiddleware({
+  target: process.env.REACT_APP_JAEGER_URL,
+  changeOrigin: true,
+  logLevel: 'debug',
+  pathRewrite: { '^/tracing': '/api/v2/spans' }
+}));
+
+app.use('/users', createProxyMiddleware({
+  target: process.env.REACT_APP_USERS_URL,
+  changeOrigin: true,
+  logLevel: 'debug'
+}));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
