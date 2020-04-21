69b47ec07f89983c3bba86c2cf7f563152a86ce4
Carlos Nunez <carlosonunez@noreply.github.com>
2020-04-21 18:51:19 -0500

    Get logging working in the frontend.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { logger } from '../Middleware/Logging'
+import { logger } from './Middleware/Logging'
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
@@ -12,10 +12,3 @@ ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-app.post('/log-client-errors', (req, res) => {
-    let error       = req.body.error.message;
-    let errorInfo   = req.body.error.stack;
-    logger.error(`The app received a new client log: ${error} ${errorInfo}`);
-    res.status(200);
-}); 
